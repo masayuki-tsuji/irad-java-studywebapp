c63158fc9327856378cde371edc56832fc5338d6
EC2 Default User <ec2-user@ip-172-31-47-111.ap-southeast-1.compute.internal>
2018-07-12 04:24:27 +0000

    講座用ファイルセッティング

--- playapp/app/models/Accounts.java
+++ playapp/app/models/Accounts.java
@@ -4,14 +4,3 @@ import io.ebean.*;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
-@Entity
-public class Accounts extends Model {
-    @Id
-    public Long id;
-    
-    public String name;
-    
-    public String address;
-    
-    public boolean status;
-}
